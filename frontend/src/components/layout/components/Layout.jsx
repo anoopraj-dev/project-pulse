f1c80c8d7759666ed6fff0f43afef301178deb2f
Anoop Raj <cutelittleanu123@gmail.com>
2026-01-28 00:17:31 +0530

    FIX:sidebar toggle and responsiveness

--- frontend/src/components/layout/components/Layout.jsx
+++ frontend/src/components/layout/components/Layout.jsx
@@ -1,8 +1,8 @@
-
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import { useUser } from "../../../contexts/UserContext";
+import { useState } from "react";
 
 import {
   adminSidebarMenu,
@@ -10,12 +10,11 @@ import {
   patientSidebarMenu,
 } from "../configs/sidebarConfig";
 
-
-const NAVBAR_HEIGHT = "h-16";
-const SIDEBAR_WIDTH = "w-64";
+const NAVBAR_HEIGHT = "h-16"; // navbar height
 
 const Layout = () => {
   const { role } = useUser();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const config =
     role === "patient"
@@ -25,20 +24,35 @@ const Layout = () => {
       : adminSidebarMenu;
 
   return (
-    <div className="h-screen overflow-hidden">
+    <div className="h-screen overflow-hidden relative">
+      {/* Navbar */}
       <header className={`fixed top-0 left-0 right-0 z-50 ${NAVBAR_HEIGHT}`}>
-        <Navbar />
+        <Navbar toggleSidebar={() => setIsSidebarOpen((prev) => !prev)} />
       </header>
 
+      {/* Sidebar */}
       <aside
-        className={`fixed top-16 left-0 ${SIDEBAR_WIDTH} bottom-0 z-40`}
+        className={`
+          fixed top-18 left-0 bottom-0 z-20 flex flex-col bg-[#0096C7] text-white
+          transition-all duration-300 shadow-lg
+          ${isSidebarOpen ? "w-64" : "w-20"}
+        `}
       >
-        <Sidebar config={config} />
+        <Sidebar
+          config={config}
+          isOpen={isSidebarOpen}
+          toggleSidebar={setIsSidebarOpen}
+        />
       </aside>
 
+      {/* Main content */}
       <main
-        className={`ml-64 mt-16 h-[calc(100vh-4rem)] overflow-y-auto bg-gray-50`}
-      > 
+        className={`
+          mt-16 h-[calc(100vh-4rem)] overflow-y-auto bg-gray-50 
+          transition-all duration-300
+          ${!isSidebarOpen ? "ml-20" : "ml-0"}
+        `}
+      >
         <Outlet />
       </main>
     </div>
--- frontend/src/components/layout/components/Navbar.jsx
+++ frontend/src/components/layout/components/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-0 w-full z-50 backdrop-blur-md bg-white/95 shadow-sm border-b border-slate-100">
+    <nav className="fixed top-0 w-full z-60 backdrop-blur-md bg-white/95 shadow-sm border-b border-slate-100">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between py-3 sm:py-4">
           {/* Left: Logo + Mobile hamburger */}
