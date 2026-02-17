a1249466417cb4fb2959228f47c1155eb932196c
Anoop Raj <cutelittleanu123@gmail.com>
2026-02-18 02:43:02 +0530

    CHORE: login image, authCard style

--- frontend/src/components/auth/components/AuthImage.jsx
+++ frontend/src/components/auth/components/AuthImage.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const AuthImage = () => {
   return (
-    <div className="relative w-full h-screen flex justify-center items-center overflow-hidden">
-  <img
-    src="./loginbanner.jpg"
-    alt="auth banner"
-    className="w-full h-full object-cover object-center"
-   
-  />
-</div>
+    <div className="relative w-full h-screen flex justify-center items-center overflow-hidden ">
+      {/* Image */}
+      <img
+        src="./steth.jpg"
+        alt="auth banner"
+        className="w-full h-full object-cover object-left "
+      />
+
+      {/* Gradient overlay */}
+      <div className="absolute inset-0 rounded-xl bg-gradient-to-r from-transparent via-black/30 to-black/60" />
+    </div>
   );
 };
 
 export default AuthImage;
+
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -20,3 +20,5 @@ const SignIn = () => {
 }
 
 export default SignIn;
+
+
